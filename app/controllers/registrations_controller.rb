39bcb33710dac7b0976695e789e4149cdcfb9314
Haseeb Chaudhary <haseebchaudhary@gmail.com>
2024-05-15 17:34:28 +0200

    controllers added

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < ApplicationController
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      # For additional fields in app/views/devise/registrations/new.html.erb
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname company])
+  
+      # For additional in app/views/devise/registrations/edit.html.erb
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname lastname company])
+    end
+  
+    def after_sign_up_path_for(_resource)
+      check_full_report_path(session[:last_check_id], anchor: 'report-banner-1') if session[:last_check_id]
+    end
+
+end
